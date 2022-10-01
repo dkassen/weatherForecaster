1686ca9c7b58a5e894d79c02beacbef34c0b5764
Daniel Kassen <danny.kassen@gmail.com>
2022-10-01 13:46:26 -0700

    location service and specs

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -1,20 +1,29 @@
 require 'csv'
 
 class LocationService
-  def self.load_data!
-    return true if defined? @@locations_by_zip_code
-    csv_path = Rails.root.join('lib/data/US_ZIP_codes_to_longitude_and_latitude.csv')
-    rows = ::CSV.read csv_path
+  class NotFoundError < StandardError; end
+  # initialize the class with data a hash of zip-code to location info
+  # that will not change and should not be accessible from the outside
+  @@locations_by_zip_code = begin
+    path = Rails.root.join('lib/data/US_ZIP_codes_to_longitude_and_latitude.csv')
+    rows = ::CSV.read path, headers: true
     locations = rows.map do |row|
       Location.new(
-        latitude: row['Latitude'],
-        longitude: row['Longitude'],
+        latitude: row['Latitude'].to_f,
+        longitude: row['Longitude'].to_f,
         state: row['State'],
         city: row['City'],
-        zip_code: row['Zip'],
+        zip_code: row['Zip'].to_i,
       )
     end
-    @@locations_by_zip_code = locations.index_by(&:zip_code)
-    true
+    locations.index_by(&:zip_code)
+  end
+
+  def self.find_by_zip_code(zip_code)
+    location = @@locations_by_zip_code[zip_code.to_i]
+
+    return location if location.present?
+
+    raise NotFoundError.new "location not found for zip code #{zip_code.inspect}"
   end
 end
--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -1,7 +1,27 @@
 RSpec.describe LocationService do
-  describe '.load_data!' do
-    it do
-      described_class.load_data!
+  describe '.find_by_zip_code' do
+    subject { described_class.find_by_zip_code(zip_code) }
+
+    context 'when the supplied with a valid zip code' do
+      let(:zip_code) { 90210 }
+
+      it { is_expected.to be_a Location }
+    end
+
+    context 'when the supplied zip code is a string' do
+      let(:zip_code) { '90210' }
+
+      it { is_expected.to be_a Location }
+    end
+
+    context 'when the supplied zip code is not valid' do
+      let(:zip_code) { 'right behind you 👀' }
+
+      it 'raises an error' do
+        expect { subject }.to raise_error LocationService::NotFoundError do |error|
+          expect(error.message).to eq 'location not found for zip code "right behind you 👀"'
+        end
+      end
     end
   end
 end
